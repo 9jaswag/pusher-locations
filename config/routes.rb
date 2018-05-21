8560d3abf3bb63194797b09d5a03eeba550a9001
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-21 19:55:19 +0100

    - generate trips controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'trips/index'
+
+  get 'trips/new'
+
+  get 'trips/create'
+
+  get 'trips/show'
+
+  get 'trips/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
