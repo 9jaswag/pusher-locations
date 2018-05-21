f622d828e2c29b4abaa8e3ff72042ef2e23f503c
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-21 20:44:22 +0100

    - require bootstrap - add styling - add header

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  get 'trips/index'
-
-  get 'trips/new'
-
-  get 'trips/create'
-
-  get 'trips/show'
-
-  get 'trips/update'
+  resources :trips
+  root 'trips#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
