628ec49b50a23914ac53bd46760fa453780555b7
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-22 17:12:41 +0100

    - add pusher - update location via pusher

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,3 +1,8 @@
 class Checkin < ApplicationRecord
   belongs_to :trip
+  after_commit :notify_pusher, on: [:create]
+
+  def notify_pusher
+    Pusher.trigger('location', 'new', self.as_json)
+  end
 end
